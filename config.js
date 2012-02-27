25773246aeacccac3b1c7409acea31374aad6c22
Vladimir Elistratov <vvoovv.projects@gmail.com>
2012-02-27 23:52:04 +0400

    adding airports.profile.js and config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+var dojoConfig = {
+	async: true,
+	packages: [
+		{name: "dojo", location: "../dojo"},
+		{name: "dijit", location: "../dijit"},
+		{name: "djeo", location: "../djeo"},
+		{name: "djeo-gmaps", location: "../djeo-gmaps"},
+		{name: "djeo-ge", location: "../djeo-ge"},
+		{name: "djeo-ymaps", location: "../djeo-ymaps"},
+		{name: "djeo-demos", location: "../djeo-demos"},
+		{name: "djeo-jenks", location: "../djeo-jenks"},
+		{name: "dgrid", location: "../dgrid"},
+		{name: "xstyle", location: "../xstyle"},
+		{name: "put-selector", location: "../put-selector" }
+	]
+};
